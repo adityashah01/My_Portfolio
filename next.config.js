125a516eef778e2acb2ecd088931112994425397
adityashah01 <aditya.shh15@gmail.com>
2025-11-16 08:56:11 +0545

    1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enables static export
-};
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = nextConfig;
